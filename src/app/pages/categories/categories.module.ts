875f04258c29f3d57a4438972636e97b5ad6f11b
Talesb <talesbatistafig@hotmail.com>
2019-03-23 23:16:22 -0300

    Implementando componente formulário de categoria

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -5,12 +5,14 @@ import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryService } from './shared/services/category.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [CategoryService]
 })
