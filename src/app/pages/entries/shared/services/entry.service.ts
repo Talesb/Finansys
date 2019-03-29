01c0f33eadc39b73500235cdb359575285ffb9be
Talesb <talesbatistafig@hotmail.com>
2019-03-28 23:03:18 -0300

    Adaptando servico para responder relacionamentos

--- src/app/pages/entries/shared/services/entry.service.ts
+++ src/app/pages/entries/shared/services/entry.service.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { Entry } from '../model/entry.model';
 
 import { map, catchError, flatMap } from 'rxjs/operators';
+import { CategoryService } from 'src/app/pages/categories/shared/services/category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ import { map, catchError, flatMap } from 'rxjs/operators';
 export class EntryService {
   private apiPath = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -29,18 +30,38 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    // Observable, o flatmap ira achatar e irá restornar o observable interno
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        // Observable contendo um entry
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        )
+      })
     )
+
+
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = this.apiPath + '/' + entry.id;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        )
+      })
     )
+
+
+
   }
 
 
@@ -60,7 +81,7 @@ export class EntryService {
     jsonData.forEach(element => {
       const entry = Object.assign(new Entry(), element);
       entries.push(entry);
-    }); 
+    });
     return entries;
   }
 
