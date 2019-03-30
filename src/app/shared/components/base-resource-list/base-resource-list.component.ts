a9485c0e4c03e02b29523948c0f082a5534527e2
Talesb <talesbatistafig@hotmail.com>
2019-03-30 19:49:41 -0300

    Implementando Base Resource List

---
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -0,0 +1,30 @@
+import { OnInit } from '@angular/core';
+import { BaseResourceService } from '../../services/base-resource.service';
+import { BaseResourceModel } from '../../model/base-resource.model';
+
+
+
+export abstract class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
+
+    constructor(private resourceService: BaseResourceService<T>) { }
+
+
+    resources: T[] = [];
+
+    ngOnInit() {
+        this.resourceService.getAll()
+            .subscribe(resources => this.resources =
+                resources.sort((a, b) => b.id - a.id), error => alert('Erro ao carregar a lista: ' + error));
+    }
+
+    deleteResource(resource: T) {
+        const mustDelete = confirm('Deseja Realmente excluir este item?');
+        if (mustDelete) {
+            this.resourceService.delete(resource.id)
+                .subscribe(
+                    () => this.resources = this.resources.filter(element => element !== resource),
+                    error => alert('Erro ao tentar excluir: ' + error));
+        }
+    }
+
+}
