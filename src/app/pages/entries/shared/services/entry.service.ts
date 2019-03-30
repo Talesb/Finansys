bcef1ffaee7aa679b741a1e56338c8ce5a3370c7
Talesb <talesbatistafig@hotmail.com>
2019-03-30 17:31:09 -0300

    Adicionando ajustes no EntryService

--- src/app/pages/entries/shared/services/entry.service.ts
+++ src/app/pages/entries/shared/services/entry.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { Entry } from '../model/entry.model';
 import { CategoryService } from 'src/app/pages/categories/shared/services/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,28 +11,16 @@ import { Observable } from 'rxjs';
 export class EntryService extends BaseResourceService<Entry> {
 
   constructor(protected injector: Injector, private categoryService: CategoryService) {
-    super('api/entries', injector, Entry.fromJson)
+    super('api/entries', injector, Entry.fromJson);
   }
 
   create(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
 
-    // Observable, o flatmap ira achatar e irá restornar o observable interno
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        // Observable contendo um entry
-        return super.create(entry);
-      })
-    )
   }
 
   update(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.update(entry);
-      })
-    )
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
   protected jsonDataToResources(jsonData: any[]): Entry[] {
@@ -48,4 +36,13 @@ export class EntryService extends BaseResourceService<Entry> {
     return Entry.fromJson(jsonData);
   }
 
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
+    )
+  }
 }
