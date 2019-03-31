e91fb56b8d330f44a6a4430772fd1bd04e8420f0
Talesb <talesbatistafig@hotmail.com>
2019-03-31 19:00:39 -0300

    Ajustando bug componente de relatório

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -21,11 +21,13 @@ export class ReportsComponent implements OnInit {
 
   chartOptions = {
     scales: {
-      yAxes: [
-        { ticks: { beginAtZero: true } }
-      ]
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
     }
-  }
+  };
 
   categories: Category[] = [];
   entries: Entry[] = [];
@@ -33,64 +35,66 @@ export class ReportsComponent implements OnInit {
   @ViewChild('month') month: ElementRef = null;
   @ViewChild('year') year: ElementRef = null;
 
-
   constructor(private entryService: EntryService, private categoryService: CategoryService) { }
 
   ngOnInit() {
     this.categoryService.getAll()
-      .subscribe(categories => this.categories = categories)
+      .subscribe(categories => this.categories = categories);
   }
 
 
   generateReports() {
-    let month = this.month.nativeElement.value;
-    let year = this.month.nativeElement.value;
+    const month = this.month.nativeElement.value;
+    const year = this.year.nativeElement.value;
 
-    if (!month || !year) {
-      alert('Você precisa selecionar o mÊs e o ano para gerar os relatórios');
-    } else {
-      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this));
-    }
+    if(!month || !year)
+      alert('Você precisa selecionar o Mês e o Ano para gerar os relatórios')
+    else
+      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this))
   }
 
-  private setValues(entries: Entry[]) {
+
+  private setValues(entries: Entry[]){
     this.entries = entries;
     this.calculateBalance();
     this.setChartData();
   }
 
-  private calculateBalance() {
+
+  private calculateBalance(){
     let expenseTotal = 0;
     let revenueTotal = 0;
 
     this.entries.forEach(entry => {
-      if (entry.type == 'revenue')
+      if(entry.type == 'revenue')
         revenueTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' })
       else
         expenseTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' })
     });
 
-    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL' });
-    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL' });
-    this.balance = currencyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL' });
+    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL'});
+    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL'});
+    this.balance = currencyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL'});
   }
 
 
   private setChartData() {
-    this.revenueChartData = this.getChartData('revenue', 'Gráfico de Receitas', '#9ccc65');
-    this.expenseChartData = this.getChartData('expense', 'Gráfico de Desepesas', '#e03131');
+    this.revenueChartData = this.getChartData('revenue', 'Gráfico de Receitas', '#9CCC65');
+    this.expenseChartData = this.getChartData('expense', 'Gráfico de Despesas', '#e03131');
   }
 
-  getChartData(entryType: string, title: string, bgColor: string) {
+
+  private getChartData(entryType: string, title: string, color: string) {
     const chartData = [];
 
     this.categories.forEach(category => {
-      const filteredEntries = this.entries
-        .filter(entry => (entry.categoryId == category.id) && (entry.type == entryType));
+      // filtering entries by category and type
+      const filteredEntries = this.entries.filter(
+        entry => (entry.categoryId == category.id) && (entry.type == entryType)
+      );
 
-
-      //  Se achou lançamentos, some os valores a adciona no chartdata
-      if (filteredEntries.length > 0) {
+      // if found entries, then sum entries amount and add to chartData
+      if(filteredEntries.length > 0){
         const totalAmount = filteredEntries.reduce(
           (total, entry) => total + currencyFormatter.unformat(entry.amount, { code: 'BRL' }), 0
         )
@@ -99,7 +103,6 @@ export class ReportsComponent implements OnInit {
           categoryName: category.name,
           totalAmount: totalAmount
         })
-        console.log(chartData);
       }
     });
 
@@ -107,11 +110,9 @@ export class ReportsComponent implements OnInit {
       labels: chartData.map(item => item.categoryName),
       datasets: [{
         label: title,
-        backgroundColor: bgColor,
+        backgroundColor: color,
         data: chartData.map(item => item.totalAmount)
       }]
     }
-
-
   }
 }
